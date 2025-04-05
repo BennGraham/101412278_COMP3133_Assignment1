eace22ce343ddea2a59084632ce1159bcafce315
Benn <benngraham90@gmail.com>
2025-04-04 23:46:23 -0400

    update

--- index.js
+++ index.js
@@ -10,25 +10,17 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const PORT = process.env.PORT || 4000;
-const DB_NAME = "COMP3133";
-const DB_USER_NAME = "benngraham";
-const DB_PASSWORD = "4smI10a94mao2aNm";
-const CLUSTER_ID = "i1w2f";
-const DB_CONNECTION = `mongodb+srv://${DB_USER_NAME}:${DB_PASSWORD}@cluster0.${CLUSTER_ID}.mongodb.net/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
-const mongodb_atlas_url = process.env.MONGODB_URL || DB_CONNECTION;
+const mongodb_atlas_url = process.env.MONGODB_URL;
 
 const connectDB = async () => {
   try {
     mongoose
-      .connect(mongodb_atlas_url, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
+      .connect(mongodb_atlas_url)
       .then((success) => {
         console.log("Success Mongodb connection");
       })
       .catch((err) => {
-        console.log("Error Mongodb connection");
+        console.log("Error Mongodb connection:", err.message);
       });
   } catch (error) {
     console.log(`Unable to connect to DB : ${error.message}`);
@@ -46,9 +38,7 @@ app.use("*", cors());
 
 server.applyMiddleware({ app });
 
-app.listen({ port: PORT }, () => {
-  console.log(
-    `Server started at http://localhost:${PORT}${server.graphqlPath}`
-  );
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server started at http://0.0.0.0:${PORT}${server.graphqlPath}`);
   connectDB();
 });
